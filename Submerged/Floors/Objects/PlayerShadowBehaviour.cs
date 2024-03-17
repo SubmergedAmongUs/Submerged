5ef090de4c529839d2cecf071cafd96eb3ac9f8f
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2024-03-17 05:10:15 +0100

    Revert PlayerShadowBehaviour to original for consistency

--- Submerged/Floors/Objects/PlayerShadowBehaviour.cs
+++ Submerged/Floors/Objects/PlayerShadowBehaviour.cs
@@ -2,153 +2,62 @@
 using System.Collections.Generic;
 using System.Linq;
 using Reactor.Utilities.Attributes;
-using Submerged.Extensions;
 using Submerged.Map;
 using UnityEngine;
 
 namespace Submerged.Floors.Objects;
 
 [RegisterInIl2Cpp]
-[Obsolete("This component is obsolete and will be removed in a future update. Please use GenericShadowBehaviour instead.", true)]
+[Obsolete("This component is no longer used in Submerged and might be removed in a future update. Please use GenericShadowBehaviour instead.", true)]
 public sealed class PlayerShadowBehaviour(nint ptr) : MonoBehaviour(ptr)
 {
     public GameObject shadowObj;
     public SpriteRenderer shadowRend;
+    public SpriteRenderer playerRend;
     public PlayerControl playerControl;
+    public Sprite[] sprites;
     public Color shadowColor;
 
-    public SpriteRenderer headRend;
-    public SpriteRenderer neckRend;
-
-    // These arrays contains all the default player body sprites, but with their bottom shadows removed. The dictionaries are just a cached version of these arrays.
-    public Sprite[] shadowlessPlayerSprites;
-    public Sprite[] shadowlessAprilFoolsPlayerSprites;
-    private readonly Dictionary<Sprite, Sprite> _spritesDict = [];
-    private readonly Dictionary<Sprite, Sprite> _aprilFoolsSpritesDict = [];
+    private readonly Dictionary<Sprite, Sprite> _spritesDict = new();
 
     public void Start()
     {
         Camera.main!.cullingMask = 1073969927; // yay magic numbers
-
         shadowObj = new GameObject("Submerged Shadow") { layer = 4 };
         shadowRend = shadowObj.AddComponent<SpriteRenderer>();
-        shadowObj.transform.SetParent(transform);
+        shadowObj.transform.parent = transform;
         shadowObj.transform.localPosition = Vector3.zero;
-
-        GameObject neckObj = new("Neck Shadow") { layer = 4 };
-        neckObj.transform.SetParent(shadowObj.transform);
-        neckRend = neckObj.AddComponent<SpriteRenderer>();
-        neckRend.transform.localPosition = Vector3.zero;
-        neckRend.transform.localScale = new Vector3(1f, 25f, 1f);
-
-        GameObject headObj = new("Head Shadow") { layer = 4 };
-        headObj.transform.SetParent(shadowObj.transform);
-        headRend = headObj.AddComponent<SpriteRenderer>();
-        headRend.transform.localPosition = new Vector3(0f, 1.9f, 0f);
-
+        shadowObj.transform.localScale = AprilFoolsMode.ShouldHorseAround() ? new Vector3(0.6734f * 0.5f, 0.6734f * 0.5f, 1f) : new Vector3(0.5f, 0.5f, 1f);
         shadowColor = shadowRend.color;
+        sprites = SubmarineStatus.instance.minigameProperties.sprites;
 
-        shadowlessPlayerSprites = SubmarineStatus.instance.minigameProperties.sprites;
-        shadowlessAprilFoolsPlayerSprites = SubmarineStatus.instance.aprilFoolsShadowSpritesHolder.sprites;
+        playerRend = playerControl.cosmetics.currentBodySprite.BodySprite;
     }
 
     public void LateUpdate()
     {
-        if (!playerControl || !playerControl.cosmetics.currentBodySprite.BodySprite.enabled || playerControl.Data.IsDead)
+        if (playerControl == null) return;
+        bool shouldBeActive = playerRend.enabled && !playerControl.Data.IsDead;
+        shadowObj.SetActive(shouldBeActive);
+
+        if (!shouldBeActive) return;
+
+        Sprite sprite = playerRend.sprite;
+        Sprite newSprite = null;
+
+        if (sprite && !_spritesDict.TryGetValue(sprite, out newSprite))
         {
-            shadowObj.SetActive(false);
-            return;
+            string spriteName = sprite ? sprite.name : "LMAO NO SPRITE PRETTY CRING";
+            newSprite = sprites.FirstOrDefault(s => s.name == spriteName);
+            _spritesDict.Add(sprite, newSprite);
         }
 
-        UpdateSprites();
-        UpdateScaleAndColor();
-        shadowObj.SetActive(true);
-    }
+        if (!newSprite) newSprite = sprite;
 
-    private void UpdateSprites()
-    {
-        shadowRend.sprite = GetBodyShadowSprite(playerControl.cosmetics.currentBodySprite.BodySprite.sprite, playerControl.cosmetics.bodyType);
-
-        if (playerControl.MyPhysics.bodyType is PlayerBodyTypes.Long or PlayerBodyTypes.LongSeeker)
-        {
-            LongBoiPlayerBody longPlayerBody = playerControl.GetComponentInChildren<LongBoiPlayerBody>();
-
-            if (!longPlayerBody)
-            {
-                Error("Long player body not found! Reverting to regular player shadow.");
-                return;
-            }
-
-            if (longPlayerBody.isSeekerHorse)
-            {
-                // TODO
-                return;
-            }
-
-            neckRend.sprite = longPlayerBody.neckSprite.sprite;
-            headRend.sprite = longPlayerBody.headSprite.sprite;
-        }
-        else
-        {
-            neckRend.sprite = headRend.sprite = null;
-        }
-    }
-
-    private void UpdateScaleAndColor()
-    {
-        float scaleModifier = playerControl.MyPhysics.bodyType == PlayerBodyTypes.Horse ? 0.3367f : 0.5f;
-        shadowObj.transform.localScale = Vector3.one * scaleModifier;
-
-        shadowRend.SetColorAlpha(playerControl.cosmetics.currentBodySprite.BodySprite.color.a);
-
-        bool flipX = playerControl.cosmetics.currentBodySprite.BodySprite.flipX;
-        shadowRend.flipX = flipX;
-        neckRend.flipX = flipX;
-        headRend.flipX = flipX;
-    }
-
-    private Sprite GetBodyShadowSprite(Sprite bodySprite, PlayerBodyTypes bodyType)
-    {
-        if (!bodySprite) return null;
-
-        if (bodyType != PlayerBodyTypes.Normal)
-        {
-            if (tryGetBodyShadowSpriteInList(bodySprite, shadowlessAprilFoolsPlayerSprites, _aprilFoolsSpritesDict, out Sprite aprilFoolsResult)) return aprilFoolsResult;
-        }
-
-        if (tryGetBodyShadowSpriteInList(bodySprite, shadowlessPlayerSprites, _spritesDict, out Sprite result))
-        {
-            return result;
-        }
-
-        return bodySprite;
-
-        static bool tryGetBodyShadowSpriteInList(Sprite spriteToGet, Sprite[] allSprites, Dictionary<Sprite, Sprite> cachedSprites, out Sprite result)
-        {
-            if (!spriteToGet)
-            {
-                result = null;
-                return false;
-            }
-
-            if (cachedSprites.TryGetValue(spriteToGet, out Sprite cachedShadowSprite) && cachedShadowSprite)
-            {
-                result = cachedShadowSprite;
-                return true;
-            }
-
-            string spriteName = spriteToGet.name;
-            Sprite newShadowSprite = allSprites.FirstOrDefault(s => s.name == spriteName);
-
-            if (newShadowSprite)
-            {
-                cachedSprites[spriteToGet] = newShadowSprite;
-                result = newShadowSprite;
-                return true;
-            }
-
-            result = null;
-            return false;
-        }
+        shadowRend.sprite = newSprite;
+        Color col = shadowColor;
+        col.a = playerRend.color.a;
+        shadowRend.color = col;
+        shadowRend.flipX = playerRend.flipX;
     }
 }
