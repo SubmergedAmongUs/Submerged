00f979f60bd4931800d5bcb7a372d66b1d62e0ad
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2024-03-17 05:07:23 +0100

    Add support for longboi anonymity

---
+++ Submerged/Floors/Objects/LongPlayerRelativeShadowRenderer.cs
@@ -0,0 +1,39 @@
+﻿using Reactor.Utilities.Attributes;
+using UnityEngine;
+
+namespace Submerged.Floors.Objects;
+
+[RegisterInIl2Cpp]
+public class LongPlayerRelativeShadowRenderer(nint ptr) : RelativeShadowRenderer(ptr)
+{
+    public LongBoiPlayerBody body;
+
+    protected override void Start()
+    {
+        base.Start();
+        body = target.GetComponentInParent<LongBoiPlayerBody>();LayerMask.NameToLayer("Players");
+    }
+
+    protected override void LateUpdate()
+    {
+        base.LateUpdate();
+
+        if (targetRenderer)
+        {
+            switch (targetRenderer.name)
+            {
+                case "LongNeck":
+                    shadowRenderer.size = new Vector2(targetRenderer.size.x, 1.1f);
+                    break;
+
+                case "ForegroundNeck":
+                    shadowRenderer.size = new Vector2(targetRenderer.size.x, 1.7f);
+                    break;
+
+                case "LongHead":
+                    shadowRenderer.transform.localPosition = new Vector3(shadowRenderer.transform.localPosition.x, body.neckSprite.transform.localPosition.y + 2.79f, shadowRenderer.transform.localPosition.z);
+                    break;
+            }
+        }
+    }
+}
