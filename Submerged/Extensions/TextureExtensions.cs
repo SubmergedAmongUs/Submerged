4479873f08a1eaacab1606bf2cdaf1ba76468ad0
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2024-07-31 14:07:42 +0300

    Add debug button to extract map sprites
    Not really useful for submerged but idrc lol

---
+++ Submerged/Extensions/TextureExtensions.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Submerged.Extensions;
+
+public static class TextureExtensions
+{
+    public static Texture2D GetReadable(this Texture2D texture)
+    {
+        if (texture.isReadable) return texture;
+
+        Texture2D copy = new(texture.width, texture.height);
+        //Graphics.CopyTexture(tex, copy); // copies GPU to GPU, we can't read that here in CPU land
+
+        RenderTexture tmp = RenderTexture.GetTemporary(texture.width, texture.height, 0, RenderTextureFormat.Default, RenderTextureReadWrite.sRGB);
+        Graphics.Blit(texture, tmp);
+
+        RenderTexture prev = RenderTexture.active;
+        RenderTexture.active = tmp;
+
+        copy.ReadPixels(new Rect(0, 0, tmp.width, tmp.height), 0, 0);
+        copy.Apply();
+
+        RenderTexture.active = prev;
+        RenderTexture.ReleaseTemporary(tmp);
+
+        return copy;
+    }
+}
