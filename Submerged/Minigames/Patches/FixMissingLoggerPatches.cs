4d9b50df69dc39b855985b0375b334d9377cd277
PHGaming121 <76562427+PHGaming121@users.noreply.github.com>
2025-01-18 14:19:18 -0300

    ALR, THAT SHOULD WORK NOW
    its EXACTLY HOW IT WAS IN MY CODE

--- Submerged/Minigames/Patches/FixMissingLoggerPatches.cs
+++ Submerged/Minigames/Patches/FixMissingLoggerPatches.cs
@@ -5,11 +5,12 @@ namespace Submerged.Minigames.Patches;
 [HarmonyPatch]
 public static class MinigameBeginPatch
 {
+    private static readonly Logger logger = new global::Logger("Minigame", global::Logger.Level.Info, global::Logger.Category.Gameplay);
     [HarmonyPatch(typeof(Minigame), nameof(Minigame.Begin))]
     [HarmonyPrefix]
     public static bool Prefix(Minigame __instance)
     {
-        __instance.logger ??= new Logger("Minigame", Logger.Level.Info, Logger.Category.Gameplay);
+        __instance.logger = logger;
         return true;
     }
 }
