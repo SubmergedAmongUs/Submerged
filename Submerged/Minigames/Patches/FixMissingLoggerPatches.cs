3b660996a0e5859481793d88dfc7c55a88317bd2
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2025-01-30 17:53:09 +0200

    Fix #180 - thanks for the PR @PHGaming121
    Co-Authored-By: PHGaming121 <76562427+PHGaming121@users.noreply.github.com>

--- Submerged/Minigames/Patches/FixMissingLoggerPatches.cs
+++ Submerged/Minigames/Patches/FixMissingLoggerPatches.cs
@@ -5,10 +5,12 @@ namespace Submerged.Minigames.Patches;
 [HarmonyPatch]
 public static class MinigameBeginPatch
 {
+    private static readonly Logger _logger = new("Minigame", Logger.Level.Info, Logger.Category.Gameplay);
+
     [HarmonyPatch(typeof(Minigame), nameof(Minigame.Begin))]
     [HarmonyPrefix]
     public static void Prefix(Minigame __instance)
     {
-        __instance.logger ??= new Logger("Minigame", Logger.Level.Info, Logger.Category.Gameplay);
+        __instance.logger ??= _logger;
     }
 }
